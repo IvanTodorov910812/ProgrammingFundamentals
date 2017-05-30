c6008ef94d544a131a02a067d9efebe70e71f25f
IvanTodorov <ivantodorov910812@gmail.com>
2017-05-30 20:06:25 +0300

    C# Intro

---
+++ Csharp Intro Programming/Hotel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Hotel
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            var studioPrice = 0.0;
+            var doublePrice = 0.0;
+            var suitePrice = 0.0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    studioPrice = 50;
+                    doublePrice = 65;
+                    suitePrice = 75;
+
+                    if (nights > 7)
+                    {
+                        studioPrice *= 0.95; 
+                    }   
+                    break;
+                case "June":
+                case "September":
+                    studioPrice = 60;
+                    doublePrice = 72;
+                    suitePrice = 82;
+
+                    if (nights > 14)
+                    {
+                        doublePrice *= 0.90;
+                    }
+                    break;
+                case "July":
+                case "August":
+                case "December":
+                    studioPrice = 68;
+                    doublePrice = 77;
+                    suitePrice = 89;
+
+                    if (nights > 14)
+                    {
+                        suitePrice *= 0.85;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            double studioPriceNights = studioPrice * nights;
+            double doublePriceNights = doublePrice * nights;
+            double suitePriceNights = suitePrice * nights;
+
+            if (nights > 7 && (month == "September" || month == "October"))
+            {
+                studioPriceNights -= studioPrice; 
+            }
+
+            Console.WriteLine("Studio: {0:F2} lv.", studioPriceNights);
+            Console.WriteLine("Double: {0:F2} lv.", doublePriceNights);
+            Console.WriteLine("Suite: {0:F2} lv.", suitePriceNights);
+        }
+    }
+}
---
+++ Csharp Intro Programming/TestNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNumbers
+{
+    class TestNumbers
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var maximumSum = int.Parse(Console.ReadLine());
+
+            var totalCombinations = 0;
+            var sum = 0;
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    var number = (i * j) * 3;
+
+                    sum += number;
+                    totalCombinations++;
+
+                    if (sum >= maximumSum)
+                    {
+                        Console.WriteLine($"{totalCombinations} combinations");
+                        Console.WriteLine($"Sum: {sum} >= {maximumSum}");
+
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{totalCombinations} combinations");
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
