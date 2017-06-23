63d6a1723ff563cfe0ae3dc473cc58cd20a55ea3
IvanTodorov <ivantodorov910812@gmail.com>
2017-06-23 14:48:01 +0300

    ObjectsAndClasses

---
+++ ObjectAndClasses/DayOfWeek.cs
@@ -0,0 +1,16 @@
+﻿namespace DayOfWeek
+{
+    using System;
+    using System.Globalization;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            string inputDate = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(inputDate, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
+
