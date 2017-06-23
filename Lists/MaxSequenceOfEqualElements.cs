4df357f1f888b9620a52566aac3319a1fcef239f
IvanTodorov <ivantodorov910812@gmail.com>
2017-06-23 14:20:29 +0300

    Lists

---
+++ Lists/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,56 @@
+﻿namespace MaxSequenceOfEqualElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MaxSequenceOfEqualElements
+    {
+        public static void Main()
+        {
+
+            var inputArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var maxSequenceOfEqualElements = FindMaxSequenceOfEqualElements(inputArr);
+
+            Console.WriteLine(string.Join(" ", maxSequenceOfEqualElements));
+        }
+
+        static int[] FindMaxSequenceOfEqualElements(int[] arr)
+        {
+            var longestSequence = new List<int>();
+            var currentSequence = new List<int>();
+            currentSequence.Add(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var currentNum = arr[i];
+                var searchNum = currentSequence[0];
+
+                if (currentNum == searchNum)
+                {
+                    currentSequence.Add(currentNum);
+                }
+                else
+                {
+                    if (currentSequence.Count > longestSequence.Count)
+                    {
+                        longestSequence = new List<int>(currentSequence); // this line will take all the elements from currentSequence and add it in longestSequence 
+                    }
+                    currentSequence.Clear(); // Delete all elements from the List -> Same like currentSequence = new List<int>();
+                    currentSequence.Add(currentNum);
+                }
+            }
+
+            if (currentSequence.Count > longestSequence.Count)
+            {
+                longestSequence = new List<int>(currentSequence); // this line will take all the elements from currentSequence and add it in longestSequence 
+            }
+
+            return longestSequence.ToArray();
+        }
+    }
+}
---
+++ Lists/SumReservedNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SumReservedNumbers
+{
+   public class SumReservedNumbers
+    {
+        public static void Main()
+        {
+            string[] inputNumbers = Console.ReadLine().Split();
+
+            int sum = 0;
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                string currentNumber = inputNumbers[i];
+
+                List<char> reversedNumbers = currentNumber.Reverse().ToList();
+
+                string reversedNumberString = string.Join("" ,reversedNumbers);
+                int resultNumber = int.Parse(reversedNumberString);
+                sum += resultNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
