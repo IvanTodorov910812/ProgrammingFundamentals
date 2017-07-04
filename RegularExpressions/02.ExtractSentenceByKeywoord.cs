a39ac5ce78c6141dca1bbdd7e50be80f71747c46
IvanTodorov <ivantodorov910812@gmail.com>
2017-07-04 11:36:11 +0300

    Regular Expressions

---
+++ RegularExpressions/02.ExtractSentenceByKeywoord.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.RegExercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public class ExtractSentence
+    {
+        public static void Main()
+        {
+            string word = Console.ReadLine();
+            string[] text = Console.ReadLine()
+                .Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries));
+
+            var pattern = "\\b" + "to" + "\\b"; // WORD BOUNDARY
+            Regex regex = new Regex(pattern);
+
+            foreach (var sentence in text)
+            {
+                if (regex.IsMatch(sentence))
+                {
+                    Console.WriteLine(sentence.Trim());
+                }
+            }
+        }
+    }
+}
---
+++ RegularExpressions/ExtractEmails.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.ExtractEmails
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([\w-.]+\@[a-zA-Z-]+)(\.[a-zA-Z-]+)+";
+
+        
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                string machtString = match.ToString();
+
+                if (!(machtString.StartsWith("-") ||
+                    machtString.StartsWith("_") ||
+                    machtString.StartsWith(".") ||
+                    machtString.EndsWith("-") ||
+                    machtString.EndsWith("_") ||
+                    machtString.EndsWith(".")))
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    } 
+}
